72aa53719445117d6f68d2f01781a1220521d11f
Malinda Mosholder <m.m.mosholder@gmail.com>
2021-12-15 22:29:26 -0500

    helping db params

--- config/database.php
+++ config/database.php
@@ -49,7 +49,7 @@ return [
             'port' => getenv("JAWSDB_URL") ? parse_url(getenv("JAWSDB_URL"))["port"] : env('DB_PORT', '3306'),
             'database' => getenv("JAWSDB_URL") ? substr(parse_url(getenv("JAWSDB_URL"))["path"], 1) : env('DB_DATABASE'),
             'username' => getenv("JAWSDB_URL") ? parse_url(getenv("JAWSDB_URL"))["user"] : env('DB_USERNAME'),
-            'password' => getenv("JAWSDB_URL") ? parse_url(getenv("JAWSDB_URL"))["pass"] : env('DB_PASSWORD'),
+            'password' => getenv("JAWSDB_URL") ? parse_url(getenv("JAWSDB_URL"))["pass"] : env('DB_PASSWORD', 'password'),
             'unix_socket' => env('DB_SOCKET', ''),
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
