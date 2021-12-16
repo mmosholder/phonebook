5d0ffd4fb014c5deec73439938d3fa5206af3960
Malinda Mosholder <m.m.mosholder@gmail.com>
2021-12-16 00:16:04 -0500

    finally fix env, db config

--- config/database.php
+++ config/database.php
@@ -45,11 +45,12 @@ return [
 
         'mysql' => [
             'driver' => 'mysql',
-            'host' => getenv("JAWSDB_URL") ? parse_url(getenv("JAWSDB_URL"))["host"] : env('DB_HOST', 'localhost'),
-            'port' => getenv("JAWSDB_URL") ? parse_url(getenv("JAWSDB_URL"))["port"] : env('DB_PORT', '3306'),
-            'database' => getenv("JAWSDB_URL") ? substr(parse_url(getenv("JAWSDB_URL"))["path"], 1) : env('DB_DATABASE'),
-            'username' => getenv("JAWSDB_URL") ? parse_url(getenv("JAWSDB_URL"))["user"] : env('DB_USERNAME'),
-            'password' => getenv("JAWSDB_URL") ? parse_url(getenv("JAWSDB_URL"))["pass"] : env('DB_PASSWORD', 'password'),
+            'url' => env('DATABASE_URL'),
+            'host' => env('DB_HOST', '127.0.0.1'),
+            'port' => env('DB_PORT', '3306'),
+            'database' => env('DB_DATABASE', 'forge'),
+            'username' => env('DB_USERNAME', 'forge'),
+            'password' => env('DB_PASSWORD', ''),
             'unix_socket' => env('DB_SOCKET', ''),
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
